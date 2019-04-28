5c92576294badf3eb410aae3265d62af18c68092
Mikołaj Pniak <mikolaj.pniak@lekta.ai>
2019-04-28 19:13:43 +0200

    WIP setting up, docker rdy

--- node-app/server.js
+++ node-app/server.js
@@ -1,11 +1,25 @@
 const mongoose = require('mongoose');
 const express = require('express');
 const bodyParser = require('body-parser');
-var cors = require('cors');
 
-const APP_PORT = 6969;
+const { 
+    SESS_TIMEOUT = 10,
+    APP_PORT = 6969
+} = process.env;
+
 const app = express();
-app.use(cors());
-const router = express.router();
+const router = express.Router();
 
-//connect with backend's database.
+//put our middlewares into app object
+var middlewares = require('./middlewares/index.js');
+//use cookie session with expiration time
+app.use(middlewares);
+
+app.get('/', (req, res) => {
+    console.log("req");
+    res.send('Hai there');
+});
+
+app.listen(APP_PORT, () => {
+    console.log(`App is listening on port ${APP_PORT}`);
+});
